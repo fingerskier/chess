5ba8fda96b0f092803fc346863de984807bfb362
fingerskier <fingerskier@gmail.com>
2025-04-10 17:02:56 -0500

    pre-cleanup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # chess
 Chess
+
+
+## TODO
+
+1-player
+2-player
+Trainer
+Custom board
+Custom pieces
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from './com/Footer'
+import Header from './com/Header'
+import Main from './com/Main'
+
+import './App.css'
+
+
+export default function App() {
+  return <>
+    <Header />
+    
+    <Main />
+    
+    <Footer />
+  </>
+}
